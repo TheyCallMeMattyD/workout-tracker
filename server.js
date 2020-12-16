683b307c396a446614045c4d006aff411f418a23
TheMattyDGWU <themattydgwu@gmail.com>
2020-12-16 16:31:21 -0500

    deploying

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+
 });
 
 require("./routes/apiRoutes")(app);
